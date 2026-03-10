80bc36500fe1768f5fb1bb7d039143a8781a3ee6
Shahulshibin <shahulshibintp@gmail.com>
2026-03-10 12:53:09 +0530

    update backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,45 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "civictrack-db.cxs86y4eiwti.ap-south-1.rds.amazonaws.com ",
-  user: "admin",
-  password: "YOURPASSWORD",
-  database: "civicsolverdb"
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-  } else {
-    console.log("Connected to AWS RDS");
-  }
-});
-
-module.exports = db;
-
-const sqlite3 = require("sqlite3").verbose()
-const path = require("path")
-
-const dbPath = path.join(__dirname, "../database/civictrack.db")
-
-const db = new sqlite3.Database(dbPath, (err) => {
-
-if(err){
-console.error("Database error:", err.message)
-}
-else{
-console.log("Connected to SQLite database")
-}
-
-})
-
-const mysql = require("mysql2");
-
-const db = mysql.createConnection({
   host: "civictrack-db.cxs86y4eiwti.ap-south-1.rds.amazonaws.com",
   user: "admin",
   password: "Admin1234",
-  database: "civicsolver-db"
+  database: "civicsolverdb"
 });
 
 db.connect((err) => {
